649d5cf7e357b89a4184c100ea760889a1eeaa55
cy <yuxia5611@gmail.com>
2025-04-27 11:03:18 +0800

    feat:ads

--- go.mod
+++ go.mod
@@ -3,30 +3,24 @@ module sdk-go-example-google-ads
 go 1.24.0
 
 require (
-	github.com/bogdanfinn/tls-client v1.9.1
-	github.com/scrapeless-ai/scrapeless-actor-sdk-go v0.0.1
+	github.com/PuerkitoBio/goquery v1.10.3
+	github.com/scrapeless-ai/scrapeless-actor-sdk-go v0.0.2
 	github.com/sirupsen/logrus v1.9.3
+	github.com/tidwall/gjson v1.18.0
 )
 
 require (
-	github.com/andybalholm/brotli v1.1.1 // indirect
-	github.com/bogdanfinn/fhttp v0.5.34 // indirect
-	github.com/bogdanfinn/utls v1.6.5 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/cloudflare/circl v1.5.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/klauspost/compress v1.17.11 // indirect
-	github.com/quic-go/quic-go v0.48.1 // indirect
 	github.com/redis/go-redis/v9 v9.7.3 // indirect
-	github.com/tam7t/hpkp v0.0.0-20160821193359-2b70b4024ed5 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/thoas/go-funk v0.9.3 // indirect
-	github.com/tidwall/gjson v1.18.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
-	golang.org/x/net v0.37.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
 	google.golang.org/grpc v1.71.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/scrapeless-ai/scrapeless-actor-sdk-go/scrapeless"
-	proxy2 "github.com/scrapeless-ai/scrapeless-actor-sdk-go/scrapeless/proxy"
+	proxyModel "github.com/scrapeless-ai/scrapeless-actor-sdk-go/scrapeless/proxy"
 	log "github.com/sirupsen/logrus"
+	"github.com/tidwall/gjson"
+	"io"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type RequestParam struct {
@@ -27,6 +36,22 @@ var (
 	client *http.Client
 )
 
+var (
+	PlatformMapping = map[string]int{
+		"PLAY":     1,
+		"MAPS":     2,
+		"SEARCH":   3,
+		"SHOPPING": 4,
+		"YOUTUBE":  5,
+	}
+
+	creativeFormatMapping = map[string]int{
+		"text":  1,
+		"image": 2,
+		"video": 3,
+	}
+)
+
 func main() {
 	actor := scrapeless.New(scrapeless.WithProxy(), scrapeless.WithStorage())
 	defer actor.Close()
@@ -36,13 +61,14 @@ func main() {
 		panic(err)
 	}
 	// Get proxy
-	proxy, err := actor.Proxy.Proxy(context.TODO(), proxy2.ProxyActor{
+	proxy, err := actor.Proxy.Proxy(context.TODO(), proxyModel.ProxyActor{
 		Country:         "us",
 		SessionDuration: 10,
 	})
 	if err != nil {
 		panic(err)
 	}
+	//proxy = "http://group_scraper_google_trneds:c8d2279d492a@pm-gw-us.scrapeless.io:24125"
 	parse, err := url.Parse(proxy)
 	if err != nil {
 		panic(err)
@@ -55,18 +81,76 @@ func main() {
 }
 
 func doCrawl(actor *scrapeless.Actor, param *RequestParam) {
+	var (
+		region       = "anywhere"
+		adsResultAll AdsResultAll
+	)
+	if param.Region != "" {
+		region = param.Region
+	}
 	// get data
 	data, err := getCookie(context.Background(), param)
 	if err != nil {
 		log.Error(err)
 		return
 	}
+	body, err := buildForm(*param)
+
+	urlStr := "https://adstransparency.google.com/anji/_/rpc/SearchService/SearchCreatives"
+	urlForm := &url.Values{}
+	urlForm.Set("f.req", body)
+	request, err := http.NewRequest(http.MethodPost, urlStr, strings.NewReader(urlForm.Encode()))
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	request.Header.Set("accept", "*/*")
+	request.Header.Set("Content-Length", "*/*")
+	request.Header.Set("Host", "*/*")
+	request.Header.Set("Connection", "keep-alive")
+	request.Header.Set("content-type", "application/x-www-form-urlencoded")
+	request.Header.Set("origin", "https://adstransparency.google.com")
+	// 需要动态改
+	request.Header.Set("referer", fmt.Sprintf("https://adstransparency.google.com/advertiser/%s?region=%s", param.AdvertiserId, region))
+	request.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36")
+	request.Header.Set("cookie", data)
+	do, err := client.Do(request)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	all, _ := io.ReadAll(do.Body)
+	defer do.Body.Close()
+	results, nextToken, totalCount := parseData(string(all))
+	for i, result := range results {
+		detailsLink := buildDetailsLink(*param, result)
+		results[i].DetailsLink = detailsLink
+		if results[i].Format == "" {
+			results[i].Format = "text"
+		} else {
+			results[i].Format = param.CreativeFormat
+		}
+	}
+	resp := []AdsResult{}
+	if param.PoliticalAds == "true" {
+		for _, result := range results {
+			if result.Political {
+				resp = append(resp, result)
+			}
+		}
+	} else {
+		resp = results
+	}
+	adsResultAll.AdCreatives = resp
+	adsResultAll.NextPageToken = nextToken
+	adsResultAll.TotalResults = totalCount
+	resultBytes, _ := json.Marshal(adsResultAll)
 	// Store data
 	// You can use bucketId to store data in a specific Object
 	// Example:
 	// 			sl.Storage.GetObject("bucketId").Put(...)
-
-	objectId, err := actor.Storage.GetObject().Put(context.TODO(), "coockie.json", []byte(data))
+	log.Info(string(resultBytes))
+	objectId, err := actor.Storage.GetObject().Put(context.TODO(), fmt.Sprintf("ads-%s.json", param.AdvertiserId), resultBytes)
 	if err != nil {
 		log.Error(err)
 		return
@@ -100,7 +184,7 @@ func getCookie(ctx context.Context, info *RequestParam) (string, error) {
 	return cookie, nil
 }
 func httpGetCookie(ctx context.Context, url string) (scrapeRes string, err error) {
-	request, err := http.NewRequest(http.MethodGet, url, nil)
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		log.Printf("request err:%v", err)
 		return "", err
@@ -129,4 +213,194 @@ func httpGetCookie(ctx context.Context, url string) (scrapeRes string, err error
 		return "", err
 	}
 	return resp.Header.Get("Set-Cookie"), nil
+}
+
+func buildForm(ads RequestParam) (string, error) {
+	num, _ := strconv.Atoi(ads.Num)
+	data := make(map[string]any)
+	if num == 0 {
+		num = 40
+	}
+	data["2"] = num
+	data3 := make(map[string]any)
+	region := strings.Split(ads.Region, ",")
+	regionInt := []int{}
+	for _, s := range region {
+		if s == "" {
+			continue
+		}
+		atoi, _ := strconv.Atoi(s)
+		regionInt = append(regionInt, atoi)
+	}
+	if len(regionInt) != 0 {
+		data3["8"] = regionInt
+	}
+	if ads.Platform != "" {
+		platform := strings.Split(ads.Platform, ",")
+		platformArray := []int{}
+		for _, s := range platform {
+			if _, ok := PlatformMapping[s]; !ok {
+				return "", errors.New("参数异常")
+			}
+			platformArray = append(platformArray, PlatformMapping[s])
+		}
+
+		data3["14"] = platformArray
+	}
+	if ads.CreativeFormat != "" {
+		if _, ok := creativeFormatMapping[ads.CreativeFormat]; !ok {
+			return "", errors.New("参数异常")
+		}
+		data3["4"] = creativeFormatMapping[ads.CreativeFormat]
+	}
+	if ads.NextPageToken != "" {
+		data["4"] = ads.NextPageToken
+	}
+	if ads.StartDate != "" && ads.EndDate != "" {
+		start, err := time.Parse("20060102", ads.StartDate)
+		if err != nil {
+			return "", err
+		}
+		end, err := time.Parse("20060102", ads.EndDate)
+		if err != nil {
+			return "", err
+		}
+		if !start.Before(end) {
+			return "", err
+		}
+		startDate, _ := strconv.Atoi(ads.StartDate)
+		endDate, _ := strconv.Atoi(ads.EndDate)
+		//date := map[string]int{"6": startDate, "7": endDate}
+		data3["6"] = startDate
+		data3["7"] = endDate
+	}
+	if ads.AdvertiserId != "" {
+		advertiserIds := strings.Split(ads.AdvertiserId, ",")
+		data3["13"] = map[string]any{
+			"1": advertiserIds,
+		}
+	}
+	if ads.Text != "" {
+		data3["12"] = map[string]any{
+			"1": ads.Text,
+			"2": true,
+		}
+	} else {
+		data3["12"] = map[string]any{
+			"1": "",
+			"2": true,
+		}
+	}
+	data["3"] = data3
+	data["7"] = map[string]int{
+		"1": 1,
+		"2": 0,
+		"3": 2156,
+	}
+	marshal, _ := json.Marshal(data)
+	return string(marshal), nil
+}
+
+type AdsResultAll struct {
+	AdCreatives   []AdsResult `json:"ad_creatives"`
+	TotalResults  int64       `json:"total_results,omitempty"`
+	NextPageToken string      `json:"next_page_token,omitempty"`
+}
+
+type AdsResult struct {
+	AdvertiserId       string `json:"advertiser_id"`
+	Advertiser         string `json:"advertiser"`
+	AdCreativeId       string `json:"ad_creative_id"`
+	Format             string `json:"format"` // 这个值与入参creative_format保持一致
+	Image              string `json:"image,omitempty"`
+	Width              int64  `json:"width,omitempty"`
+	Height             int64  `json:"height,omitempty"`
+	TotalDaysShown     int64  `json:"total_days_shown"`
+	FirstShown         int64  `json:"first_shown"`
+	LastShown          int64  `json:"last_shown"`
+	MinimumViewsCount  int64  `json:"minimum_views_count,omitempty"`
+	MaximumViewsCount  int64  `json:"maximum_views_count,omitempty"`
+	MiniMumBudgetSpent string `json:"mini_mum_budget_spent,omitempty"`
+	MaxMumBudgetSpent  string `json:"max_mum_budget_spent,omitempty"`
+	DetailsLink        string `json:"details_link"`
+	Political          bool   `json:"-"`
+}
+
+func parseData(data string) ([]AdsResult, string, int64) {
+	parse := gjson.Parse(data)
+	nextToken := parse.Get("2").String()
+	m := parse.Map()
+	countIndex := 0
+	for s, _ := range m {
+		maxI, _ := strconv.Atoi(s)
+		if maxI > countIndex {
+			countIndex = maxI
+		}
+	}
+	totalCount := parse.Get(fmt.Sprintf("%d", countIndex)).Int()
+
+	results := parse.Get("1").Array()
+	adsResults := make([]AdsResult, 0)
+	for _, r := range results {
+		advertiserId := r.Get("1").String()
+		adCreativeId := r.Get("2").String()
+		advertiser := r.Get("12").String()
+		imagesDom := r.Get("3.3.2").String()
+		dc, _ := goquery.NewDocumentFromReader(strings.NewReader(imagesDom))
+		img := dc.Find("img")
+		image, _ := img.Attr("src")
+		heightStr, _ := img.Attr("height")
+		height, _ := strconv.Atoi(heightStr)
+		widthStr, _ := img.Attr("width")
+		width, _ := strconv.Atoi(widthStr)
+
+		firstShown := r.Get("6.1").Int()
+		lastShown := r.Get("7.1").Int()
+		totalDaysShown := r.Get("13").Int()
+		minimumViewsCount := r.Get("8").Int()
+		maxmumViewsCount := r.Get("9").Int()
+		result := AdsResult{
+			AdvertiserId:      advertiserId,
+			Advertiser:        advertiser,
+			AdCreativeId:      adCreativeId,
+			Image:             image,
+			Width:             int64(width),
+			Height:            int64(height),
+			TotalDaysShown:    totalDaysShown,
+			FirstShown:        firstShown,
+			LastShown:         lastShown,
+			MinimumViewsCount: minimumViewsCount,
+			MaximumViewsCount: maxmumViewsCount,
+		}
+		if r.Get("10.1").Exists() {
+			miniMumBudgetSpent := r.Get("10.1").Array()[0].Get("2").String() + " " + r.Get("10.1").Array()[0].Get("1").String()
+			result.MiniMumBudgetSpent = miniMumBudgetSpent
+		}
+		if r.Get("11.1").Exists() {
+			maxMumBudgetSpent := r.Get("11.1").Array()[0].Get("2").String() + " " + r.Get("11.1").Array()[0].Get("1").String()
+			result.MaxMumBudgetSpent = maxMumBudgetSpent
+		}
+		if r.Get("15").Exists() {
+			result.Political = true
+		}
+		adsResults = append(adsResults, result)
+	}
+	return adsResults, nextToken, totalCount
+}
+
+func buildDetailsLink(info RequestParam, r AdsResult) string {
+	resp := fmt.Sprintf("https://adstransparency.google.com/advertiser/%s/creative/%s?region=%s", r.AdvertiserId, r.AdCreativeId, info.Region)
+	if info.CreativeFormat != "" {
+		resp += fmt.Sprintf("&format=%s", strings.ToUpper(info.CreativeFormat))
+	}
+	if info.StartDate != "" && info.EndDate != "" {
+		start, _ := time.Parse("20060102", info.StartDate)
+		end, _ := time.Parse("20060102", info.EndDate)
+		resp += fmt.Sprintf("&start-date=%s", start.Format(time.DateOnly))
+		resp += fmt.Sprintf("&end-date=%s", end.Format(time.DateOnly))
+	}
+	if info.Platform != "" {
+		resp += fmt.Sprintf("&platform=%s", strings.ToUpper(info.Platform))
+	}
+	return resp
 }
